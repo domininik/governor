4b764a2bc4fbd53c8c02c49f47c05b270f298541
domininik <dominik.fijas+github@gmail.com>
2023-06-25 19:46:37 +0200

    Add voting tests

--- test/MyGovernor.t.sol
+++ test/MyGovernor.t.sol
@@ -18,9 +18,9 @@ contract MyGovernorTest is Test {
     token = new GovToken();
     governor = new MyGovernor(token);
 
-    targets = [address(0x123)];
+    targets = [address(token)];
     values = [0];
-    calldatas = [abi.encode("transfer", address(0x456), 1000)];
+    calldatas = [abi.encode("transfer", address(0x456), 500)];
     description = "Proposal #1: Give grant to team";
   }
 
@@ -42,6 +42,22 @@ contract MyGovernorTest is Test {
 
   function test_ProposalThreshold() public {
     assertEq(governor.proposalThreshold(), 0);
+  }
+
+  function test_GetVotes() public {
+    address owner = token.owner();
+
+    token.mint(owner, 1000);
+    assertEq(token.balanceOf(owner), 1000);
+    assertEq(token.numCheckpoints(owner), 0);
+    assertEq(governor.getVotes(owner, 0), 0);
+
+    token.delegate(owner);
+    assertEq(token.numCheckpoints(owner), 1);
+    assertEq(governor.getVotes(owner, 0), 0);
+
+    vm.roll(2);
+    assertEq(governor.getVotes(owner, 1), 1000);
   }
 
   function test_Propose() public {
@@ -68,9 +84,20 @@ contract MyGovernorTest is Test {
     assertEq(uint256(governor.state(proposalId)), uint256(IGovernor.ProposalState.Canceled));
   }
 
-  function testFail_CastVote() public {
-    vm.expectRevert("Governor: vote not currently active");
+  function test_TooLate_Cancel() public {
+    governor.propose(
+      targets,
+      values,
+      calldatas,
+      description
+    );
 
+    vm.roll(10000);
+    vm.expectRevert("Governor: too late to cancel");
+    governor.cancel(targets, values, calldatas, keccak256(bytes(description)));
+  }
+
+  function test_TooEarly_CastVote() public {
     uint256 proposalId = governor.propose(
       targets,
       values,
@@ -78,10 +105,16 @@ contract MyGovernorTest is Test {
       description
     );
 
-    governor.castVote(proposalId, 1);
+    vm.expectRevert("Governor: vote not currently active");
+    governor.castVote(proposalId, uint8(GovernorCountingSimple.VoteType.For));
   }
 
   function test_CastVote() public {
+    address owner = token.owner();
+
+    token.mint(owner, 1000);
+    token.delegate(owner);
+
     uint256 proposalId = governor.propose(
       targets,
       values,
@@ -92,6 +125,14 @@ contract MyGovernorTest is Test {
     vm.roll(10000);
     assertEq(uint256(governor.state(proposalId)), uint256(IGovernor.ProposalState.Active));
 
-    governor.castVote(proposalId, 1);
+    governor.castVote(proposalId, uint8(GovernorCountingSimple.VoteType.For));
+
+    assertEq(governor.hasVoted(proposalId, address(this)), true);
+    
+    (uint256 againstVotes, uint256 forVotes, uint256 abstainVotes) = governor.proposalVotes(proposalId);
+
+    assertEq(againstVotes, 0);
+    assertEq(forVotes, 1000);
+    assertEq(abstainVotes, 0);
   }
 }
